7cfea0c42e787924c8449955c817034a6cb94e3d
Sourabh Singh <sourabhrishi25@gmail.com>
2017-06-19 16:35:26 +0530

    Added the Traffic Engine

---
+++ TrafficEngine/TrafficSettings.cpp
@@ -0,0 +1,16 @@
+#include "TrafficSettings.h"
+using namespace Traffic;
+
+const unsigned int TrafficSettings::SCREEN_HEIGHT = 1080;
+const unsigned int TrafficSettings::SCREEN_WIDTH = 1920;
+const float CAMERA_POSX		= 0.0f;
+const float CAMERA_POSY		= 0.0f;
+const float CAMERA_POSZ		= 20.0f;
+const float CAMERA_FACEX	= 0.0f;
+const float CAMERA_FACEY	= 0.0f;
+const float CAMERA_FACEZ	= 0.0f;
+const float CAMERA_UPX		= 0.0f;
+const float CAMERA_UPY		= 1.0f;
+const float CAMERA_UPZ		= 0.0f;
+
+
---
+++ TrafficEngine/TrafficSettings.h
@@ -0,0 +1,18 @@
+#pragma once
+namespace Traffic {
+	class TrafficSettings
+	{
+	public:
+		static const unsigned int SCREEN_WIDTH;
+		static const  unsigned int SCREEN_HEIGHT;
+		static const float CAMERA_POSX		;	
+		static const float CAMERA_POSY		;	
+		static const float CAMERA_POSZ		;	
+		static const float CAMERA_FACEX		;	
+		static const float CAMERA_FACEY		;	
+		static const float CAMERA_FACEZ		;	
+		static const float CAMERA_UPX		;	
+		static const float CAMERA_UPY		;	
+		static const float CAMERA_UPZ		;	
+	};
+}
